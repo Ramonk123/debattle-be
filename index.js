89b4bdb2cb4145cf5c49b2535d3d80d64bd2773b
RamonK <ramonkrijnen10@outlook.com>
2023-04-26 14:38:40 +0200

    Added new packages + more .gitignore

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 
+const {MongoClient} = require('mongodb');
+const database = require('./util/database.js');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+database.connect()
+    .then(() => {
+      app.listen(port, () => {
+        console.log('Server listening on port 3000');
+      });
+    })
+    .catch((err) => {
+      console.log('Failed to connect to MongoDB server', err);
+      process.exit(1);
+    });
+
+
+
---
+++ util/database.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+
+const url = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:27017/${process.env.DATABASE_NAME}`;
+console.log(url);
+
+let client = null;
+
+function connect() {
+    return MongoClient.connect(url).then((_client) => {
+        client = _client;
+        console.log("Connected successfully to MongoDB server");
+    }).catch((err) => {
+        console.log("Failed to connect to MongoDB server", err);
+        throw err;
+    });
+}
+
+function getClient() {
+    if (!client) {
+        throw new Error('MongoDB client not initialized');
+    }
+
+    return client;
+}
+
+module.exports = {
+    connect,
+    getClient
+};
+
