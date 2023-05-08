c6fbfadf58757a5950bc3d14006feb09f4535bfd
RamonK <ramonkrijnen10@outlook.com>
2023-05-08 14:58:55 +0200

    Added functionality for deleting questions, cleaned up code and improved error handling messages.

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -4,14 +4,17 @@ const {ObjectId} = require('mongodb');
 
 exports.getQuestion = async (req, res) => {
     const dbo = getClient();
-    const questionId = new ObjectId(req.path.substring(1));
     try {
-    const question = await dbo.collection('questions').find({_id: questionId}).toArray();
+        const questionId = new ObjectId(req.path.substring(1));
+        const question = await dbo.collection('questions').find({_id: questionId}).toArray();
+        if (!question || question.length === 0) {
+            throw new Error(`No question found with id: ${questionId}`);
+        }
         return res.status(200).json(question)
     } catch (error) {
 
         console.error(error)
-        return res.status(500).json('An error occurred')
+        return res.status(404).json(error.message);
 
     }
 }
@@ -20,6 +23,9 @@ exports.getQuestions = async (req, res) => {
     const dbo = getClient();
     try {
         const questions = await dbo.collection('questions').find().toArray();
+        if (questions.length === 0) {
+            throw new Error('No questions found');
+        }
         return res.status(200).json(questions);
     } catch (error) {
 
@@ -33,8 +39,8 @@ exports.addQuestion = async (req, res) => {
     const body = req.body;
 
     try {
-        if (!Array.isArray(body) || body.length === 0) {
-            throw new Error('Request body is not an array or is empty');
+        if (Object.keys(body).length === 0) {
+            throw new Error('Request body is empty');
         } else if (body.length === 1) {
             await dbo.collection('questions').insertOne(body[0]);
             return res.status(201).json('Question added');
@@ -44,30 +50,46 @@ exports.addQuestion = async (req, res) => {
         }
     } catch (error) {
         console.error(error)
-        return res.status(500).json('An error occurred');
+        return res.status(500).json(error.message);
     }
 }
 
 exports.editQuestion = async (req, res) => {
     const dbo = getClient();
     const body = req.body;
-    const questionId = new ObjectId(req.path.substring(1));
+    console.log(body)
 
     try {
-        if (!body) {throw new Error('No body present');}
+        const questionId = new ObjectId(req.path.substring(1));
+        if (Object.keys(body).length === 0) {
+            throw new Error('No body present');
+        }
         delete body._id;
-        dbo.collection('questions').updateOne({_id: questionId}, {$set: body});
+        const questionResult = await dbo.collection('questions').updateOne({_id: questionId}, {$set: body});
+        if (questionResult.matchedCount === 0) {
+            throw new Error('No matches found');
+        }
         return res.status(200).json('Question modified');
-
 
     } catch (error) {
         console.error(error);
-        return res.status(500).json('An error occurred');
-
-
+        return res.status(500).json(error.message);
     }
+}
 
-
+exports.removeQuestion = async (req, res) => {
+    const dbo = getClient();
+    try {
+        const questionId = new ObjectId(req.path.substring(1));
+        const questionResult = await dbo.collection('questions').deleteOne({_id: questionId});
+        if (questionResult.deletedCount === 0) {
+            throw new Error(`Question with id: ${questionId} could not be found`);
+        }
+        return res.status(200).json('Question has been deleted');
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error.message);
+    }
 }
 
 const getClient = () => {
--- routes/QuestionRoutes.js
+++ routes/QuestionRoutes.js
@@ -6,6 +6,7 @@ const questionController = require('../controllers/QuestionController');
 router.get('/:id', questionController.getQuestion);
 router.get('', questionController.getQuestions);
 router.post('', questionController.addQuestion);
-router.put('/:id', questionController.editQuestion)
+router.put('/:id', questionController.editQuestion);
+router.delete('/:id', questionController.removeQuestion);
 
 module.exports = router
